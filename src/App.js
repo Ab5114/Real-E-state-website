737295d4c3517d8603f2a373456bc837bd0403df
Ab5114 <sharmanitin5114@gmail.com>
2025-01-30 16:10:34 +0530

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,40 +1,59 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+ import { ToastContainer } from "react-toastify";
+ import { SpeedInsights } from "@vercel/speed-insights/react";
+ import { Suspense, lazy } from "react";
 
-import Header from "./Header.jsx";
-import About from "./About";
-import Projects from "./Projects.jsx";
-import Testimonials from "./Testimonials.jsx";
-import Contact from "./Contact.jsx";
-import Footer from "./Footer.jsx";
-import { ToastContainer } from "react-toastify";
-import { SpeedInsights } from "@vercel/speed-insights/react";
-import Signup from "./Signup.jsx";
-import SearchProperty from "./search-components/SearchProperty.jsx";
+  const Header = lazy(() => import("./Header.jsx"));
+ const About = lazy(() => import("./About"));
+ const Projects = lazy(() => import("./Projects.jsx"));
+ const Testimonials = lazy(() => import("./Testimonials.jsx"));
+ const Contact = lazy(() => import("./Contact.jsx"));
+ const Footer = lazy(() => import("./Footer.jsx"));
+ const Signup = lazy(() => import("./Signup.jsx"));
+  const SearchProperty = lazy(() =>
+    import("./search-components/SearchProperty.jsx")
+  );
+  
+
+
 
 function App() {
   return (
     <>
       <ToastContainer />
       <SpeedInsights />
-
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <Header />
-              <About />
-              <Projects />
-              <Testimonials />
-              <Contact />
-              <Footer />
-            </>
-          }
-        />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/search-property" element={<SearchProperty />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Suspense fallback={<div>Loading Header...</div>}>
+                  <Header />
+                </Suspense>
+                <Suspense fallback={<div>Loading About...</div>}>
+                  <About />
+                </Suspense>
+                <Suspense fallback={<div>Loading Projects...</div>}>
+                  <Projects />
+                </Suspense>
+                <Suspense fallback={<div>Loading Testimonials...</div>}>
+                  <Testimonials />
+                </Suspense>
+                <Suspense fallback={<div>Loading Contact...</div>}>
+                  <Contact />
+                </Suspense>
+                <Suspense fallback={<div>Loading Footer...</div>}>
+                  <Footer />
+                </Suspense>
+              </>
+            }
+          />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/search-property" element={<SearchProperty />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
